d0f0ebad65d2559f662b02337fcbff7ae7942c2f
Chenxiaoqi1995 <109154060+Chenxiaoqi1995@users.noreply.github.com>
2025-12-18 10:34:32 +0800

    Update diy.sh

--- N1-LEDE/diy.sh
+++ N1-LEDE/diy.sh
@@ -14,9 +14,8 @@ function git_sparse_clone() {
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone --depth=1 https://github.com/vernesong/OpenClash package/OpenClash
-git clone --depth=1 https://github.com/OldCoding/luci-app-filebrowser package/luci-app-filebrowser
-git clone --depth 1 https://github.com/zijieKwok/luci-theme-alpha package/luci-theme-alpha
-git clone --depth 1 https://github.com/derisamedia/luci-app-alpha-config package/luci-app-alpha-config
+git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone -b 18.06 --single-branch --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/ddnsgo
 #git clone --depth=1 https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
