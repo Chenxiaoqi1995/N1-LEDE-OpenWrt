464a7b76b9f570cbd20432de247369b9518b2c52
Chenxiaoqi1995 <109154060+Chenxiaoqi1995@users.noreply.github.com>
2025-12-21 23:05:39 +0800

    Change default IP address in diy-part2.sh

--- N1-LEDE/diy-part2.sh
+++ N1-LEDE/diy-part2.sh
@@ -13,7 +13,7 @@
 
 # Modify default IP   第一行19.07的路径   第二行23.05的路径
 #sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.123.2/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.3/g' package/base-files/luci2/bin/config_generate
 
 # 修改主机名
 #sed -i 's/LEDE/OpenWrt/g' package/base-files/files/bin/config_generate
